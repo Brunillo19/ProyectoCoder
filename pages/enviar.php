4258c5387b6a1e77563c6204a35559034bf1c466
Bruno Iglesias <iglesiasb11@gmail.com>
2022-07-22 21:35:27 -0300

    Prueba formulario extra 3

--- pages/enviar.php
+++ pages/enviar.php
@@ -4,17 +4,19 @@ $nombre = $_POST['nombre'];
 $numero = $_POST['telefono'];
 $email = $_POST['email'];
 $caja = $_POST['caja'];
-
+$dia = date("d/m/y", time());
 
 
 $mensaje = <<<MAIL
 El usuario $nombre, con número de teléfono $numero, ha realizado un comentario en tu sitio web. <br>
-Comentario: $caja
+Comentario: $caja <br>
+Enviado el $dia
 MAIL;
 
 $informacion_adicional = 'Mime-Version: 1.0'. ",\r\n";
 $informacion_adicional .= 'Content-type: text/html; charset=UTF-8'. ",\r\n";
 $informacion_adicional .= 'From: '. $nombre. '<'.$email.'>' ;
+
 
 
 mail('iglesiasb11@gmail.com', 'Comentario en BI Fotografía', utf8_decode($mensaje), $informacion_adicional);
