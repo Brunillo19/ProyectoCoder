ffd66c8b14275eb6850613b4368dbf3efa84ce5b
Bruno Iglesias <iglesiasb11@gmail.com>
2022-07-23 12:17:51 -0300

    Prueba formulario 4

--- pages/enviar.php
+++ pages/enviar.php
@@ -4,13 +4,11 @@ $nombre = $_POST['nombre'];
 $numero = $_POST['telefono'];
 $email = $_POST['email'];
 $caja = $_POST['caja'];
-$dia = date("d/m/y", time());
 
 
 $mensaje = <<<MAIL
 El usuario $nombre, con número de teléfono $numero, ha realizado un comentario en tu sitio web. <br>
-Comentario: $caja <br>
-Enviado el $dia
+Comentario: $caja.
 MAIL;
 
 $informacion_adicional = 'Mime-Version: 1.0'. ",\r\n";
